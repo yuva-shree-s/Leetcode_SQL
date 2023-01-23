6ee1cc3e70f0413129ffb0afee1c7c5edfb298a2
YOGESHVAR M <91490891+YogeshvarM@users.noreply.github.com>
2023-01-23 21:26:27 +0530

    Add files via upload
    Description
    LeetCode Problem 1407.
    
    Table: Users
    
    1
    2
    3
    4
    5
    6
    7
    8
    +---------------+---------+
    | Column Name   | Type    |
    +---------------+---------+
    | id            | int     |
    | name          | varchar |
    +---------------+---------+
    id is the primary key for this table.
    name is the name of the user.
    Table: Rides
    
    1
    2
    3
    4
    5
    6
    7
    8
    9
    +---------------+---------+
    | Column Name   | Type    |
    +---------------+---------+
    | id            | int     |
    | user_id       | int     |
    | distance      | int     |
    +---------------+---------+
    id is the primary key for this table.
    user_id is the id of the user who travelled the distance "distance".
    Write an SQL query to report the distance travelled by each user.
    
    Return the result table ordered by travelled_distance in descending order, if two or more users travelled the same distance, order them by their name in ascending order.
    
    The query result format is in the following example.
    
    1
    2
    3
    4
    5
    6
    7
    8
    9
    10
    11
    12
    13
    14
    15
    16
    17
    18
    19
    20
    21
    22
    23
    24
    25
    26
    27
    28
    29
    30
    31
    32
    33
    34
    35
    36
    37
    38
    39
    40
    41
    42
    43
    Users table:
    +------+-----------+
    | id   | name      |
    +------+-----------+
    | 1    | Alice     |
    | 2    | Bob       |
    | 3    | Alex      |
    | 4    | Donald    |
    | 7    | Lee       |
    | 13   | Jonathan  |
    | 19   | Elvis     |
    +------+-----------+
    
    Rides table:
    +------+----------+----------+
    | id   | user_id  | distance |
    +------+----------+----------+
    | 1    | 1        | 120      |
    | 2    | 2        | 317      |
    | 3    | 3        | 222      |
    | 4    | 7        | 100      |
    | 5    | 13       | 312      |
    | 6    | 19       | 50       |
    | 7    | 7        | 120      |
    | 8    | 19       | 400      |
    | 9    | 7        | 230      |
    +------+----------+----------+
    
    Result table:
    +----------+--------------------+
    | name     | travelled_distance |
    +----------+--------------------+
    | Elvis    | 450                |
    | Lee      | 450                |
    | Bob      | 317                |
    | Jonathan | 312                |
    | Alex     | 222                |
    | Alice    | 120                |
    | Donald   | 0                  |
    +----------+--------------------+
    Elvis and Lee travelled 450 miles, Elvis is the top traveller as his name is alphabetically smaller than Lee.
    Bob, Jonathan, Alex and Alice have only one ride and we just order them by the total distances of the ride.
    Donald didn't have any rides, the distance travelled by him is 0.

---
+++ Leetcode_1407.sql
@@ -0,0 +1,73 @@
+
+#DUMPING:
+
+CREATE TABLE Users (
+  `id` INTEGER,
+  `name` VARCHAR(8)
+);
+
+INSERT INTO Users
+  (`id`, `name`)
+VALUES
+  ('1', 'Alice'),
+  ('2', 'Bob'),
+  ('3', 'Alex'),
+  ('4', 'Donald'),
+  ('7', 'Lee'),
+  ('13', 'Jonathan'),
+  ('19', 'Elvis');
+
+CREATE TABLE Rides (
+  `id` INTEGER,
+  `user_id` INTEGER,
+  `distance` INTEGER
+);
+
+INSERT INTO Rides
+  (`id`, `user_id`, `distance`)
+VALUES
+  ('1', '1', '120'),
+  ('2', '2', '317'),
+  ('3', '3', '222'),
+  ('4', '7', '100'),
+  ('5', '13', '312'),
+  ('6', '19', '50'),
+  ('7', '7', '120'),
+  ('8', '19', '400'),
+  ('9', '7', '230');
+
+CREATE TABLE Result (
+  `name` VARCHAR(8),
+  `travelled_distance` INTEGER
+);
+
+INSERT INTO Result
+  (`name`, `travelled_distance`)
+VALUES
+  ('Elvis', '450'),
+  ('Lee', '450'),
+  ('Bob', '317'),
+  ('Jonathan', '312'),
+  ('Alex', '222'),
+  ('Alice', '120'),
+  ('Donald', '0');
+
+
+#Answer:
+
+select * from users;
+
+select * from rides;
+
+select * from result____;
+
+select u.name,coalesce(sum(r.distance),0) as total_distance
+from users as u
+left join rides as r
+on u.id=r.user_id
+group by u.name
+order by total_distance desc,u.name;
+
+
+
+
